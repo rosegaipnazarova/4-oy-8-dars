8d6cf6d9ed6719ef9fe4ec20e16c6667002daf1e
rosegaipnazarova <rosegaipnazarova@gmail.com>
2026-02-05 20:14:20 +0500

    task

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -2,19 +2,14 @@ const {v4} = require("uuid")
 
 const { read_file, write_file } = require("../api/file-system")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+
+
 const register = async(req,res) =>{
     try{
 
-    }catch(error){
-        res.status(500).json({
-            message:error.message
-        })
-    }
-}
-
-const login = async(req,res) =>{
-    try{
-        const {username, email, password} = req.body
+       const {username, email, password} = req.body
         if (!username || !email || !password) {
            return res.status(400).json({
                 message:"Username, email, password are required"
@@ -36,8 +31,55 @@ const login = async(req,res) =>{
             id: v4(),
             username,
             email,
-            password: hash
+            password: hash,
+            role: "user"
         })
+
+        write_file("auth.json", fileData)
+        res.status(201).json({
+            message : "Registered"
+        })
+
+    }catch(error){
+        res.status(500).json({
+            message:error.message
+        })
+    }
+}
+
+const login = async(req,res) =>{
+    try{
+        const {email, password} = req.body
+        if (!email || !password) {
+           return res.status(400).json({
+                message:"Email, password are required"
+            })
+        }
+
+        const fileData = read_file("auth.json")
+        const foundedUser = fileData.find((item) => item.email === email)
+
+        if (!foundedUser) {
+            return res.status(400).json({
+                message:"User not found!"
+            })
+        }
+        
+        const check = await bcrypt.compare(password, foundedUser.password)
+
+
+        if (check) {
+            const payload= {id: foundedUser.id, email: foundedUser.email, role: foundedUser.role }
+            const token = jwt.sign(payload, )
+
+            
+        }else{
+            res.status(401).json({
+                message: "wrong password"
+            })
+        }
+
+
 
         write_file("auth.json", fileData)
         res.status(201).json({
